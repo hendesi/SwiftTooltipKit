3c16524c7f0797ca69f76e238858de3b601f839f
Felix Desiderato <felix.desiderato@tum.de>
2022-03-29 12:22:20 +0200

    Update minimum project requirements

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTooltipKit",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v8)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
